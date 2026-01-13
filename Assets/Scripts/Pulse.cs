105ed658c3aadb18c31d98380e5cf0416b430dcd
evelybe <evelybe@sheridancollege.ca>
2026-01-13 10:14:25 -0500

    Created a script using an Animation Curve.

--- Assets/Scripts/Pulse.cs
+++ Assets/Scripts/Pulse.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 
-public class LinearInterpolation : MonoBehaviour
+public class Pulse : MonoBehaviour
 {
-    public Transform start;
-    public Transform end;
-    public float t;
     public AnimationCurve curve;
+    public float t;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,7 +20,7 @@ public class LinearInterpolation : MonoBehaviour
             t = 0;
         }
 
-        //transform.position = Vector2.Lerp(start.position, end.position, t);
-        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+        float y = curve.Evaluate(t);
+        transform.localScale = Vector3.one * curve.Evaluate(t);
     }
 }
