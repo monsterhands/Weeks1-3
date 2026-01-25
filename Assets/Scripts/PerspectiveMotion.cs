f0c5812b14161b0f3318fb9f25451b41230ad858
monsterhands <beinean.evely@gmail.com>
2026-01-25 10:47:23 -0500

    Final changes to pseudocode and scene

--- Assets/Scripts/PerspectiveMotion.cs
+++ Assets/Scripts/PerspectiveMotion.cs
@@ -12,7 +12,7 @@ public class PerspectiveMotion : MonoBehaviour
     public float speed;
     //set a float variable for measuring the wait time between the end and resumption of the lerp function
     public float wait;
-    //set a animation curves for affecting the motion and scale of game object
+    //set animation curves for affecting the motion and scale of game object
     public AnimationCurve curveMotion;
     public AnimationCurve curveSize;
     //set float variable for the size of the game object
@@ -29,15 +29,23 @@ public class PerspectiveMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //tbd
+        //timer needs to update by deltaTime and be changed by speed set in inspector
         timer += Time.deltaTime * speed;
 
+        //if the timer reaches a value greater than the wait time
+        //one update plus a value set in inspector
         if (timer > wait)
         {
+            //timer is reset to remove the wait period that has just occurred
             timer = timer - wait;
         } 
 
+        //the position of the affected game object is linearly interpolated from a chosen start game object position
+        //to an end game object position with animation curve motion along the value of the timer,
+        //0-1 with speed factored in
         transform.position = Vector2.Lerp(start.position, end.position, curveMotion.Evaluate(timer));
-        transform.localScale = (Vector3.one * size) * curveSize.Evaluate(timer);
+        //conversely, this animation curve changes the scale as it evaluates the value of the timer
+        //multiplying a vector3 of scale by the size set in the inspector to affect X, Y
+        transform.localScale = (Vector2.one * size) * curveSize.Evaluate(timer);
     }
 }
